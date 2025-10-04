9b19a4afab56b4ee3606a06a5eb9921fe5c94224
GrantODP <grantoranduprez@gmail.com>
2025-10-04 21:28:54 +0200

    forecast client item model & inputs (predict, load and transform)

--- forecast/models/forecast_base.py
+++ forecast/models/forecast_base.py
@@ -1,60 +1,49 @@
 from abc import ABC, abstractmethod
+from typing import Callable, Any, Optional
+from pathlib import Path
+import numpy as np
 import pandas as pd
 
+
 class ParameterAdaptor(ABC):
+    def __init__(self, features: list[str]):
+        self.features: list[str] = features
+        self.input: pd.DataFrame = pd.DataFrame()
 
-    def __init__(self, features=[])
-        self.features = features
-        sefl.input = pd.Dataframe()
-
-    def __validate_rawdata(raw_data):
+    def __validate_rawdata(self, raw_data: pd.DataFrame):
         missing = [col for col in self.features if col not in raw_data.columns]
         assert not missing, f"Missing required columns: {missing}"
-    
-    
-    def __to_dataframe(self, raw_data = {}) -> pd.Dataframe:
-        if raw_data isintance(raw_data, pd.Dataframe)
-            __validate_rawdata(raw_data)
+
+    def to_dataframe(self, raw_data={}) -> pd.DataFrame:
+        if isinstance(raw_data, pd.DataFrame):
+            self.__validate_rawdata(raw_data)
             return raw_data
 
-        df = pd.Dataframe(raw_data)
-        __validate_rawdata(raw_data)
-
+        df: pd.DataFrame = pd.DataFrame(raw_data)
+        self.__validate_rawdata(raw_data)
         return df
-    
+
     @abstractmethod
     def transform(self, parameters):
         pass
-    
-    @abstractmethod
-    def recurse_update(self, prediction):
-        pass
 
-    def parameters(self) -> pd.Dataframe:
+    def parameters(self) -> pd.DataFrame:
         return self.input
 
 
 class ForecastModel(ABC):
-
-    @abstarctmethod
-    def load(self, path: str)
+    @abstractmethod
+    def load(self, path: str | Path, loader: Callable[[str | Path], Any] | None = None):
         pass
 
     @abstractmethod
-    def predict(self, x_params: ParameterAdaptor, steps=1):
+    def predict(self, x_params: ParameterAdaptor) -> dict[str, float]:
         pass
 
 
 class Forecaster:
+    def __init__(self, model: ForecastModel):
+        self.model: ForecastModel = model
 
-
-    def __init__(self, model):
-        self.model = model
-
-    
-    def predict(self, x_params: ParameterAdaptor, steps=1):
-        return self.model.predict(x_params, steps)
-
-
-
-
+    def predict(self, parameters: ParameterAdaptor) -> dict[str, float]:
+        return self.model.predict(parameters)
---
+++ forecast/models/forecast_models.py
@@ -0,0 +1,110 @@
+from pathlib import Path
+
+from typing import Callable, Any
+from typing import override
+import joblib
+from joblib.externals.cloudpickle.cloudpickle import k
+import pandas as pd
+import numpy as np
+
+
+from .forecast_base import ParameterAdaptor, ForecastModel
+
+
+class ClientItemAdaptor(ParameterAdaptor):
+    def __init__(self):
+        required_features = [
+            "item",
+            "date",
+            "cust_name",
+            "cust_code",
+            "price",
+            "region",
+            "area",
+            # time series features if not sent from api should be calculated from the pickle data file (should be done outside class)
+            "qty_sold_log_lag1",
+            "qty_sold_log_lag5",
+            "qty_sold_log_lag10",
+            "rolling_mean_3",
+            "rolling_std_3",
+            "rolling_mean_5",
+            "rolling_std_5",
+            "rolling_mean_10",
+            "rolling_std_10",
+            "days_since_client_purchase",
+            "days_since_client_item_purchase",
+        ]
+        self.features: list[str] = []
+        super().__init__(required_features)
+
+    @override
+    def transform(self, parameters):
+        df = self.to_dataframe(parameters)
+        df["item"] = df["item"].astype("category")
+        df["region"] = df["region"].astype("category")
+        df["area"] = df["area"].astype("category")
+        df["cust_id"] = df["cust_id"].astype("category")
+        df["cust_code"] = df["cust_code"].astype("category")
+        df["category"] = (df["color"] + "_" + df["container"]).astype("category")
+        df["color"] = df["color"].astype("category")
+        df["container"] = df["container"].astype("category")
+        df["currency"] = df["currency"].astype("category")
+
+        df["dayofweek"] = df["date"].dt.dayofweek
+        df["dayofweek_sin"] = np.sin(2 * np.pi * df["dayofweek"] / 7)
+        df["dayofweek_cos"] = np.cos(2 * np.pi * df["dayofweek"] / 7)
+        df["month"] = df["date"].dt.month
+        df["month_sin"] = np.sin(2 * np.pi * df["month"] / 12)
+        df["month_cos"] = np.cos(2 * np.pi * df["month"] / 12)
+        df["dayofyear"] = df["date"].dt.dayofyear
+        df["dayofyear_sin"] = np.sin(2 * np.pi * df["dayofyear"] / 365)
+        df["dayofyear_cos"] = np.cos(2 * np.pi * df["dayofyear"] / 365)
+        df["day"] = df["date"].dt.day
+        df["day_sin"] = np.sin(2 * np.pi * df["day"] / 30)
+        df["day_cos"] = np.cos(2 * np.pi * df["day"] / 30)
+        df["year"] = df["date"].dt.year
+        df["quarter"] = df.date.dt.quarter
+        df["quarter_sin"] = np.sin(2 * np.pi * df["quarter"] / 4)
+        df["quarter_cos"] = np.cos(2 * np.pi * df["quarter"] / 4)
+
+        df["price"] = np.log1p(df["price"])
+        self.input = df
+
+
+def default_model_loader(path: str | Path):
+    import joblib
+
+    return joblib.load(path)
+
+
+class DirectQuantileForecaster(ForecastModel):
+    def __init__(self, model_name: str, quantiles: list[int], horizon=1):
+        self.model_name: str = model_name
+        self.models: dict[str, Any] = {}
+        self.quantiles: list[int] = quantiles
+        self.horizon = horizon
+        self.__create_model_names()
+
+    def __create_model_names(self):
+        self.models = {f"q{q}": None for q in self.quantiles}
+
+    @override
+    def load(self, path: str | Path, loader: Callable[[str | Path], Any] | None = None):
+        if loader is None:
+            loader = default_model_loader
+
+        for key in self.models:
+            self.models[key] = loader(path)
+
+    @override
+    def predict(self, parameters: ParameterAdaptor) -> dict[str, float]:
+        features = parameters.parameters()
+
+        quant_predictions: dict[str, float] = {
+            q_str: model.predict(features)[0] for q_str, model in self.models.items()
+        }
+
+        for key, q in quant_predictions.items():
+            quant_predictions[key] = np.expm1(q)
+
+        return quant_predictions
