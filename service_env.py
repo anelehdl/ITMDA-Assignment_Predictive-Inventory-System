630903ec551cad6aba93d4eb7eaee7c48b2d7d5c
GrantODP <grantoranduprez@gmail.com>
2025-10-10 09:14:40 +0200

    service features and util

---
+++ service_env.py
@@ -0,0 +1,37 @@
+from pydantic import BaseSettings
+import consul
+
+from pydantic import BaseSettings, Field
+from pathlib import Path
+from typing import Any
+
+
+class ServiceEnviroment(BaseSettings):
+    host: str = Field(default="localhost", env="HOST")
+    port: str = Field(default="8420", env="PORT")
+
+    service_name: str = Field(default="predict-service", env="SRV_NAME")
+    data_dir: Path = Field(default="predict-service", env="DATA_DIR")
+
+    data_host: str = Field(default="localhost", env="DHOST")
+    data_port: str = Field(default="8520", env="DPORT")
+
+
+def register_service(
+    name, host, port, unique_id=1, consul_params: dict[str, Any] | None = None
+):
+    if not consul_params:
+        consul_params = {"host": "localhost", "port": 8500}
+
+    c = consul.Consul(**consul_params)
+    service_id = f"{name}_{port}_{unique_id}"
+
+    c.agent.service.register(
+        name=name,
+        service_id=service_id,
+        address=host,
+        port=port,
+        tags=["forecast-cached-features"],
+        check=consul.Check.tcp(host, port, interval="10s"),
+    )
+    return service_id
