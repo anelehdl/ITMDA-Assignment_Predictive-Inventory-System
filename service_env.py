627378c779c27fe1c1475914358edc7c33df4be6
GrantODP <grantoranduprez@gmail.com>
2025-10-20 18:06:10 +0200

    Documentation

--- service_env.py
+++ service_env.py
@@ -11,6 +11,17 @@ PROJECT_ROOT = Path(__file__).parent
 
 
 class ServiceEnvironment:
+    """ Enviroment variables for service
+
+    Contains all the needed variables for service hosting, directory discovery
+    and service discovery from consul
+
+    Example:
+    settings = ServiceEnvironment()
+    host = settings.host
+    """
+
+    #TODO: Replace variables with property so no need to intitialize each time
     def __init__(self):
         self.host = os.environ.get("HOST", "localhost")
         self.port = os.environ.get("PORT", "8420")
@@ -30,6 +41,22 @@ def register_service(
     tags=[],
     consul_params: Dict[str, Any] | None = None,
 ):
+    """Utiliy function for Consul registering.
+
+    Registers the current service to a consul service discovery.
+
+    Args:
+        name: The name for the service for discovery with names.
+        host: Service host.
+        port: Service port.
+        unique_id: id to randomize service with same name. (incase host and port is the same although this should be impossible)
+        tags: a list of tags to further identify services with the same name.
+        consul_params: parameters to connect to consu.l 
+
+    Returns:
+        Returns the registered id.
+    """
+
     if not consul_params:
         consul_params = {
             "host": os.getenv("CONSUL_HOST", "localhost"),
@@ -51,6 +78,19 @@ def register_service(
 
 
 def get_services(service_name, tag=None, consul_host="localhost", consul_port=8500):
+    """Retrieves the service from consul.
+
+    Registered services can be located from consul matching name pattern and specified tag.
+
+    Args:
+        service_name: Name of the service.
+        tag: Tags to narrow search with multiple same service names
+        consul_host: Host of consul service discovery
+        consul_port: Port of consul service discovery
+
+    Returns:
+        Returns a list of services that match search parameters 
+    """
     import requests
 
     try:
