74e5c578e65d529ea7dcd2b0cab9d64786de1785
Grant du Preez <grantoranduprez@gmail.com>
2025-10-22 09:02:05 +0200

    Service logging

--- forecast-service/data_service.py
+++ forecast-service/data_service.py
@@ -6,12 +6,13 @@ from nutec_forecast.util.time_series_util import (
     get_client_item_time_series_features,
     get_item_info,
 )
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 
 import dataclasses
 import pandas as pd
 import os
+import logging
 from dataclasses import dataclass
 
 
@@ -25,7 +26,14 @@ class FeatureRequest(BaseModel):
     item: str
     client_name: str
 
-
+logger = logging.getLogger(__name__)
+logger = logging.basicConfig(
+    level=logging.INFO,
+    filename="data_service.log",
+    filemode="a",
+    format='%(asctime)s - %(levelname)s - %(message)s',  
+    datefmt='%Y-%m-%d %H:%M:%S'  
+)
 service_id = 42
 settings = ServiceEnvironment()
 local_data = CachedFeatures(
@@ -47,6 +55,8 @@ def startup_event():
             "port": settings.consul_port
         }
     )
+    logging.info(f"Data Service registered at id: {reg_id}")
+    
 
 
 
@@ -56,12 +66,14 @@ def health():
 
 
 @app.post("/time-features", response_model=Dict[str, Any])
-async def get_cached_time_features(request: FeatureRequest):
+async def get_cached_time_features(request: Request, feature: FeatureRequest):
     """Endpoint to get time series related features for forecasting. 
     """
+    
+    logger.info(f"Request:/time-features ADDRESS:{request.client.host}")
     try:
         features = get_client_item_time_series_features(
-            local_data.data, request.client_name, request.item
+            local_data.data, feature.client_name, feature.item
         )
     except ValueError as ve:
         raise HTTPException(status_code=404, detail={"error": str(ve)})
@@ -69,9 +81,10 @@ async def get_cached_time_features(request: FeatureRequest):
 
 
 @app.get("/item/{item_id}")
-async def get_item(item_id: str):
+async def get_item(item_id: str, request: Request):
     """ Endpoint to get category related features for item.
     """
+    logger.info(f"Request:/item  value:{item_id} ADDRESS:{request.client.host}")
     try:
         info = get_item_info(local_data.data, item_id)
     except ValueError as ve:
